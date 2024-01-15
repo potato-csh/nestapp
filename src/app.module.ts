3cf3d898b410a6feed13c1f8bb3ac8fba756d28e
potato-csh <1103782026@qq.com>
2024-01-15 23:28:16 +0800

    core3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { database } from './config';
 import { ContentModule } from './modules/content/content.module';
+import { CoreModule } from './modules/core/core.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-    imports: [ContentModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [ContentModule, CoreModule.forRoot(), DatabaseModule.forRoot(database)],
+    providers: [],
 })
 export class AppModule {}
