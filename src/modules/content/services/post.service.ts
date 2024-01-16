e8793ea463a0ac3e2bc16996d1df6f5f1d15fe7a
potato-csh <1103782026@qq.com>
2024-01-16 19:02:03 +0800

    orm 1

--- src/modules/content/services/post.service.ts
+++ src/modules/content/services/post.service.ts
@@ -1,51 +1,64 @@
-import { NotFoundException } from '@nestjs/common';
-import { isNil } from 'lodash';
+import { Injectable } from '@nestjs/common';
 
-import { CreatePostDto } from '../dtos/create-post.dto';
-import { UpdatePostDto } from '../dtos/update-post.dto';
-import { PostEntity } from '../types';
+import { IsNull, Not, SelectQueryBuilder } from 'typeorm';
 
+import { QueryHook } from '@/modules/database/types';
+
+import { PostOrderType } from '../constants';
+import { PostEntity } from '../entities/post.entity';
+import { PostRepository } from '../repositories';
+
+@Injectable()
 export class PostService {
-    protected posts: PostEntity[] = [
-        { title: '第一篇文章标题', body: '第一篇文章内容' },
-        { title: '第二篇文章标题', body: '第二篇文章内容' },
-        { title: '第三篇文章标题', body: '第三篇文章内容' },
-        { title: '第四篇文章标题', body: '第四篇文章内容' },
-        { title: '第五篇文章标题', body: '第五篇文章内容' },
-        { title: '第六篇文章标题', body: '第六篇文章内容' },
-    ].map((v, id) => ({ ...v, id }));
+    constructor(protected respository: PostRepository) {}
 
-    async findAll() {
-        return this.posts;
+    // async paginate(options: PaginateOptions, callback?: QueryHook<PostEntity>) {
+    //     const qb = await this.
+    // }
+
+    /**
+     * 构建文章列表查询器
+     * @param qb
+     * @param options
+     * @param callback
+     */
+    protected async buildListQuery(
+        qb: SelectQueryBuilder<PostEntity>,
+        options: Record<string, any>,
+        callback?: QueryHook<PostEntity>,
+    ) {
+        const { orderBy, isPublished } = options;
+        let newQb = qb;
+        if (typeof isPublished === 'boolean') {
+            newQb = isPublished
+                ? newQb.where({ publishedAt: Not(IsNull()) })
+                : newQb.where({ publishedAt: IsNull() });
+        }
+        newQb = this.queryOrderBy(newQb, orderBy);
+        if (callback) return callback(newQb);
+        return newQb;
     }
 
-    async findOne(id: number) {
-        const post = this.posts.find((item) => item.id === Number(id));
-        if (isNil(post)) throw new NotFoundException(`the post with id ${id} no exits!`);
-        return post;
-    }
-
-    async create(data: CreatePostDto) {
-        const newPost: PostEntity = {
-            id: Math.max(...this.posts.map(({ id }) => id + 1)),
-            ...data,
-        };
-        this.posts.push(newPost);
-        return newPost;
-    }
-
-    async update(data: UpdatePostDto) {
-        let toUpdate = this.posts.find((item) => item.id === data.id);
-        if (isNil(toUpdate)) throw new NotFoundException(`the post with id ${data.id} not exits!`);
-        toUpdate = { ...toUpdate, ...data };
-        this.posts = this.posts.map((item) => (item.id === data.id ? toUpdate : item));
-        return toUpdate;
-    }
-
-    async delete(id: number) {
-        const toDelete = this.posts.find((item) => item.id === Number(id));
-        if (isNil(toDelete)) throw new NotFoundException(`the post with id ${id} no exits!`);
-        this.posts = this.posts.filter((item) => item.id !== Number(id));
-        return toDelete;
+    /**
+     * 对文章进行排序的Query构建
+     * @param qb
+     * @param orderBy
+     */
+    protected async queryOrderBy(qb: SelectQueryBuilder<PostEntity>, orderBy?: PostOrderType) {
+        switch (orderBy) {
+            case PostOrderType.CREATED:
+                return qb.orderBy('post.createdAt', 'DESC');
+            case PostOrderType.UPDATED:
+                return qb.orderBy('post.updateAt', 'DESC');
+            case PostOrderType.PUBLISHED:
+                return qb.orderBy('post.publishedAt', 'DESC');
+            case PostOrderType.CUSTOM:
+                return qb.orderBy('post.customOrder', 'DESC');
+            default:
+                return qb
+                    .orderBy('post.createdAt', 'DESC')
+                    .addOrderBy('post.updatedAt', 'DESC')
+                    .addOrderBy('post.publishedAt', 'DESC');
+        }
     }
 }
